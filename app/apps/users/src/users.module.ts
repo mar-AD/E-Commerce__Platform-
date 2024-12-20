3e6777177de40ab82d85c6304bad31a2fd3a8313
marra abdallah <abdallah4marra@gmail.com>
2024-12-21 00:26:52 +0100

    configure rmq in the users module

--- app/apps/users/src/users.module.ts
+++ app/apps/users/src/users.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { CommonModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['./apps/users/.env', './.env']
+    }),
+    CommonModule,
+
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'RMQ_CONSUMER',
+      useFactory: (configService: ConfigService) => {
+        return ClientProxyFactory.create({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL')],
+            queue: configService.get<string>('RABBITMQ_USERS_QUEUE'),
+            queueOptions: { durable: true },
+          }
+        })
+      },
+      inject: [ConfigService]
+    }
+  ],
+  exports:[ 'RMQ_CONSUMER' ]
 })
 export class UsersModule {}
